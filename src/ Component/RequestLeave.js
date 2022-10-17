a40d7d5c3dfe086d3a7864f7cf7079dbdb7773db
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2022-10-17 21:16:23 +0530

    Update : reQuest Leave is working fine with backend

---
+++ src/ Component/RequestLeave.js
@@ -0,0 +1,197 @@
+import React, { useState, useEffect } from "react";
+
+import Axios from "axios";
+
+export default function RequestLeave() {
+  const [empId, setEmpId] = useState("");
+
+  const [department, setDepartment] = useState("");
+
+  const [leaveType, setLeaveType] = useState("");
+
+  const [applicationDate, setApplicationDate] = useState("");
+
+  const [fromDate, setFromDate] = useState("");
+
+  const [toDate, setToDate] = useState("");
+
+  const [totalDays, setTotalDays] = useState("");
+
+  const [reason, setReason] = useState("");
+
+  const submitRequest = () => {
+    Axios.post("http://localhost:3001/api/insertLeaveRequest", {
+      // here new varaibles have created using old variable + adding e before the old variable
+      empId: empId,
+      department: department,
+      leaveType: leaveType,
+      applicationDate: applicationDate,
+      fromDate: fromDate,
+      toDate: toDate,
+      totalDays: totalDays,
+      reason: reason,
+    }).then(() => {
+      alert("successful insert");
+    });
+  };
+
+  return (
+    <div>
+      <h1>Leave Request</h1>
+
+      <form className="ml-2 mr-2 w">
+        {/* Empliyee id */}
+
+        <div className="form-group ">
+          <label htmlFor="InputEmpId">Employee Id</label>
+          <input
+            type="number"
+            name="empId"
+            className="form-control"
+            id="InputApplicationDate"
+            aria-describedby="emailHelp"
+            placeholder="input emp id"
+            onChange={(e) => {
+              setEmpId(e.target.value);
+              console.log(e.target.value);
+            }}
+          />
+        </div>
+
+        {/* branch */}
+        <div className="form-group ">
+          <label htmlFor="InputEmpId">Branch</label>
+          <br />
+          <select
+            name="department"
+            id=""
+            onChange={(e) => {
+              setDepartment(e.target.value);
+              console.log(e.target.value);
+            }}
+          >
+            <option value="">Input your Branch </option>
+            <option value="CS">CSE</option>
+            <option value="IT">IT</option>
+            <option value="EE">EE</option>
+            <option value="EN">EN</option>
+            <option value="EC">EC</option>
+            <option value="ME">ME</option>
+          </select>
+        </div>
+
+        {/* leave type  */}
+        <div className="form-group ">
+          <label htmlFor="InputEmpId">Branch</label>
+          <br />
+          <select
+            name="leaveType"
+            id=""
+            onChange={(e) => {
+              setLeaveType(e.target.value);
+              console.log(e.target.value);
+            }}
+          >
+            <option value="">Leave Type </option>
+            <option value="Casual">Casual</option>
+            <option value="First Half">Frist Half</option>
+            <option value="Second Half">Second Half</option>
+            <option value="One Day">One Day</option>
+          </select>
+        </div>
+
+        {/* application date */}
+        <div className="form-group ">
+          <label htmlFor="InputApplicationDate">ApplicationDate</label>
+          <input
+            type="date"
+            name="applicationDate"
+            onChange={(e) => {
+              setApplicationDate(e.target.value);
+              console.log(e.target.value);
+            }}
+            min={new Date().toISOString().split("T")[0]}
+            className="form-control"
+            id="InputApplicationDate"
+            aria-describedby="emailHelp"
+            placeholder="Input Date"
+          />
+        </div>
+
+        {/* from date */}
+        <div className="form-group ">
+          <label htmlFor="InputFromDate">FromDate</label>
+          <input
+            type="date"
+            name="fromDate"
+            onChange={(e) => {
+              setFromDate(e.target.value);
+              console.log(e.target.value);
+            }}
+            className="form-control"
+            id="InputApplicationDate"
+            aria-describedby="emailHelp"
+            placeholder="Input Date"
+          />
+        </div>
+
+        {/* to date */}
+        <div className="form-group ">
+          <label htmlFor="InputToDate">ToDate</label>
+          <input
+            type="date"
+            name="toDate"
+            onChange={(e) => {
+              setToDate(e.target.value);
+              console.log(e.target.value);
+            }}
+            className="form-control"
+            id="InputApplicationDate"
+            aria-describedby="emailHelp"
+            placeholder="Input Date"
+          />
+        </div>
+
+        {/* total Days */}
+        <div className="form-group">
+          <label htmlFor="InputDays">TotalDays</label>
+          <input
+            type="number"
+            name="totalDays"
+            onChange={(e) => {
+              setTotalDays(e.target.value);
+              console.log(e.target.value);
+            }}
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="totalDays"
+          />
+        </div>
+
+        {/* Reason section  */}
+        <div className="form-group">
+          <label htmlFor="reason">Reason</label>
+          <textarea
+            name="reason"
+            onChange={(e) => {
+              setReason(e.target.value);
+              console.log(e.target.value);
+            }}
+            type="text area"
+            className="form-control w-30"
+            id="exampleInputPassword1"
+            placeholder="Reason"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={submitRequest}
+        >
+          Apply
+        </button>
+      </form>
+    </div>
+  );
+}
