160b5ecc7932a72336d0b4e1299fbd8b766a133a
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2022-11-15 01:38:20 +0530

    created new module for faculty so that he or se will be capable to set the time table  and this module is working fine

---
+++ src/ Component/Temp.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import RequestLeave from './RequestLeave';
+import './Temp.css';
+
+
+export default function Temp() {
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary" >RequestLeave</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Request Arrangement</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+}
