4fca14323660690f707beeafbf2fbb5bf9c758df
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2022-11-15 01:34:42 +0530

    added some comment so on app.js so that all the we can get the idea of current module called and new module is called

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@
 import RequestLeave from './ Component/RequestLeave';
 import AllLeaveRequest from './ Component/AllLeaveRequest';
 import AllRequestForHod from './ Component/AllRequestForHod';
+import SetTimeTable from './ Component/SetTimeTable';
 
+// import Temp from './ Component/Temp'
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <RequestLeave />
-      {/* <AllLeaveRequest />  */}
-      <AllRequestForHod />
+
+      {/* live request for individual people */}
+      {/* <RequestLeave /> */}
+
+      {/* all leave request for individual people  */}
+      {/* <AllLeaveRequest /> */}
+
+      {/* all leave request for hod */}
+      {/* <AllRequestForHod /> */}
+
+      {/* set time table for individual people */}
+      <SetTimeTable />
+
+      {/* <Temp /> */}
+      {/* <h1>Hello how are you </h1> */}
     </div>
   );
 }
