f7e22cd4d56d766935ffbac67e7ef4202019a153
Yogi <Yogita.shukla@gmail.com>
2023-05-27 11:40:40 +0530

    New changes in project

--- backend/Requests/requestDetail.js
+++ backend/Requests/requestDetail.js
@@ -2,17 +2,23 @@ const express=require("express");
 var db = require('../sqlCredentials')
 const router=express.Router()
 
-router.get('/api/allRequestedLeave/:empId', (req, res) => {
+   
+    router.get('/api/AllRequestForHod/:requestId/:empId', (req, res) => {
     console.log(" i was executednnnn")
 
-
+    
 
     const empId = req.params.empId;
+    const requestId = req.params.requestId;
     console.log("ss=" + empId);
+    console.log("ss=" + requestId);
+    /*const sqlSelect = "SELECT *FROM leaverequest JOIN allfaculty where leaverequest.empId = allfaculty.id AND empId=1003";/api/allRequestedLeave/:empId */
 
-    const sqlSelect = "select * from LeaveRequest where empId= ? ";
+   const sqlSelect = "SELECT t1.*, t2.* FROM leaverequest t1 JOIN loginpage t2 ON t1.empId = t2.empId WHERE t1.empId = ? AND requestId = ?";
 
-    db.query(sqlSelect, [empId], (err, result) => {
+   /*const sqlSelect = "select * from loginpage where empId= ? ";*/
+
+    db.query(sqlSelect, [empId,requestId], (err, result) => {
         if (err)
             console.log(err);
         else {
--- src/ Component/EditUser/SeeAllUsers.js
+++ src/ Component/EditUser/SeeAllUsers.js
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from "react";
-import Axios from "axios";
 import "./seeusers.css";
 import "./Popupview.css";
 import axios from "axios";
@@ -17,10 +16,7 @@ function SeeAllUsers(props) {
     loadData();
   }, []);
 
-  
-    /* To search the faculty */
-  
-  
+  /* To search the faculty */
   const Filter = (event) => {
     const filteredRecords = data.filter((f) =>
       f.name.toLowerCase().includes(event.target.value.toLowerCase())
@@ -29,38 +25,36 @@ function SeeAllUsers(props) {
   };
 
   return (
-    <div  className="containerSeeUsers" id="containerSeeUsers">
+    <div className="containerSeeUsers" id="containerSeeUsers">
       <form className="formSeeUsers">
         <label className="label">
-          <h1>
-            <strong>All Users..</strong>
-          </h1>
+          <strong>All Users</strong>
         </label>
         <input
           type="text"
-          style={{ width: "1250px" }}
           className="form-control"
+          id="form-control"
           onChange={Filter}
-          placeholder="search here"
+          placeholder="search here.."
         />
         <div id="card-bodySeeUsers">
           <table className="SeeTable" id="SeeTable">
-            <tr >
-              <th className="Seetablecolheading">SNo...</th>
-              <th className="Seetablecolheading">EmpId</th>
-              <th className="Seetablecolheading">Name</th>
-              <th className="Seetablecolheading">Role</th>
-              <th className="Seetablecolheading">Email</th>
-              <th className="Seetablecolheading">Gender</th>
-              <th className="Seetablecolheading">Department</th>
-              <th className="Seetablecolheading">ContactNo</th>
-              <th className="Seetablecolheading">Date Of ..Joining</th>
+            <tr>
+              <th className="Seetablecol">SNo...</th>
+              <th className="Seetablecol">EmpId</th>
+              <th className="Seetablecol">Name</th>
+              <th className="Seetablecol">Role</th>
+              <th className="Seetablecol">Email</th>
+              <th className="Seetablecol">Gender</th>
+              <th className="Seetablecol">Department</th>
+              <th className="Seetablecol">ContactNo</th>
+              <th className="Seetablecol">Date Of ..Joining</th>
             </tr>
 
             {record.map((item, index) => {
               return (
-                <tr key={item.id}  >
-                  <td  className="Seetablecol">{index + 1}</td>
+                <tr key={item.id}>
+                  <td className="Seetablecol">{index + 1}</td>
 
                   <td className="Seetablecol">{item.empId}</td>
                   <td className="Seetablecol">{item.name}</td>
@@ -74,7 +68,7 @@ function SeeAllUsers(props) {
             })}
           </table>
         </div>
-        :<div id=""></div> 
+        :<div id=""></div>
       </form>
     </div>
   );
