22978e2d9a82cc47db71ad40702e901d2340f920
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2023-05-29 01:12:26 +0530

    saklhfkjsahfds kjsjhfkjads

--- backend/Requests/requestDetail.js
+++ backend/Requests/requestDetail.js
@@ -1,13 +1,13 @@
-const express=require("express");
+const express = require("express");
 var db = require('../sqlCredentials')
-const router=express.Router()
+const router = express.Router()
 
 
 
 // Leave Request Form
 router.post('/api/insertLeaveRequest', (req, res) => {
     const empId = req.body.empId;
-   // const department = req.body.department;
+    // const department = req.body.department;
     const leaveType = req.body.leaveType;
     const applicationDate = req.body.applicationDate;
     const fromDate = req.body.fromDate;
@@ -20,9 +20,9 @@ router.post('/api/insertLeaveRequest', (req, res) => {
     const TillprevsmonthleavetakenLWP = req.body.TillprevsmonthleavetakenLWP;
     const BalenaceleaveexcludingthisleaveCL = req.body.BalenaceleaveexcludingthisleaveCL;
     // const leaveRequest = "INSERT INTO LeaveRequest(empId, department, leaveType, applicationDate, fromDate, toDate, reason) VALUES ('4880', 'it', 'casual', '2022-03-03', '2022-03-03', '2022-03-03', 'huhuihigb iuhuig yhgygiyg y');";
-    const leaveRequest = "INSERT INTO LeaveRequest(empId, leaveType, applicationDate, fromDate, toDate, reason,LeavealrdytakncrrntmonthCL, LeavealrdytakncrrntmonthLWP,TillprevsmonthleavetakenCL,TillprevsmonthleavetakenLWP,BalenaceleaveexcludingthisleaveCL) VALUES (?,?,?,CURDATE(),?,?,?,?,?,?,?);";
+    const leaveRequest = "INSERT INTO proxy.LeaveRequest(empId, leaveType, applicationDate, fromDate, toDate, reason,LeavealrdytakncrrntmonthCL, LeavealrdytakncrrntmonthLWP,TillprevsmonthleavetakenCL,TillprevsmonthleavetakenLWP,BalenaceleaveexcludingthisleaveCL) VALUES (?,?,?,CURDATE(),?,?,?,?,?,?,?);";
     // db.query(leaveRequest, (err, result) => { await
-    db.query(leaveRequest, [empId,  leaveType, fromDate, toDate, reason,LeavealrdytakncrrntmonthCL,LeavealrdytakncrrntmonthLWP,TillprevsmonthleavetakenCL,TillprevsmonthleavetakenLWP,BalenaceleaveexcludingthisleaveCL], (err, result) => {
+    db.query(leaveRequest, [empId, leaveType, fromDate, toDate, reason, LeavealrdytakncrrntmonthCL, LeavealrdytakncrrntmonthLWP, TillprevsmonthleavetakenCL, TillprevsmonthleavetakenLWP, BalenaceleaveexcludingthisleaveCL], (err, result) => {
 
         if (err)
             console.log(err);
@@ -33,4 +33,4 @@ router.post('/api/insertLeaveRequest', (req, res) => {
 
 
 
-module.exports= router
+module.exports = router
